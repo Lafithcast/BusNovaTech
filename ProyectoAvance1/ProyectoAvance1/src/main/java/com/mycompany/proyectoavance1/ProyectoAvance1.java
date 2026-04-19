d53fb1760e86834b7098d22d5fb4529f9d156ee5
Nyyko990 <penasnicolas79@gmail.com>
2026-04-19 13:12:20 -0600

    Delete ProyectoAvance1/ProyectoAvance1 directory
    Se borro el Avance 1 para evitar confusiones

--- ProyectoAvance1/ProyectoAvance1/src/main/java/com/mycompany/proyectoavance1/ProyectoAvance1.java
+++
@@ -1,8 +0,0 @@
-//Authors = Allison Alvarado, Lafith Castrillo, Fernanda Herrera & Nicolas Peñas
-package com.mycompany.proyectoavance1;
-public class ProyectoAvance1 {
-    public static void main(String[] args) {
-        Proyecto proyecto = new Proyecto();
-        proyecto.iniciar();
-    }
-}
